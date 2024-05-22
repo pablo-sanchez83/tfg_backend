383728b3415a8adf7d1e204d951a3a7c2b874fb0
Pablo Sánchez <pablosanchezperianez83@gmail.com>
2024-05-22 11:30:09 +0200

    feat: added fields to manage.py

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from api.models import *
 
 # Register your models here.
+admin.site.register(Usuarios)
+admin.site.register(Empresas)
+admin.site.register(Categorias_Culinarias)
+admin.site.register(Locales)
+admin.site.register(Horarios)
+admin.site.register(Fotos_Locales)
+admin.site.register(Productos)
+admin.site.register(Tramos_Horarios)
+admin.site.register(Reservas)
+admin.site.register(Comentarios)
