c7e477878e1f21a93f65e74fda72ddc55d31f53a
Pablo Sanchez <pablo.sanchez@dialogotecnologico.com>
2024-06-13 17:44:15 +0200

    fix: missing comma

--- backend/settings.py
+++ backend/settings.py
@@ -24,7 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'whitenoise.runserver_nostatic'
+    'whitenoise.runserver_nostatic',
     'api',
     'rest_framework',
     'rest_framework.authtoken',
